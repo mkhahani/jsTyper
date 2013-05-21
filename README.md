9d1dbb7ad6a176f15dd8ba2db59d3e03e1b468ed
mkhahani <mkhahani@gmail.com>
2013-05-21 16:33:53 +0430

    first version

--- README.md
+++ README.md
@@ -1,4 +1,77 @@
-typer
+Typer
 =====
+#### Pure Javascript Typing Slider ####
 
-Pure Javascript Typing Slider
+Typer is a pure JavaScript utility to create typing text effect over a list of items. Typer is a good replacement for obsolete HTML "marquee". It is compatible with all major browsers (Opera, Firefox, Chrome, Safari & IE).
+
+Typer is free and publishes under the MIT license.
+
+Benefits
+--------
+* Pure JavaScript (no JS framewok or third party library usage)
+* Lightweight with optimized source code
+* Compatible with all browsers
+* Customizable using CSS
+* Easy setup
+* Multi instance support
+* Configurable per instance
+* Cool typing effect
+* Pause & play functionality on mouse moves
+* Free to use (MIT license)
+
+Usage
+-----
+Use Typer simply in two steps:
+
+1. Add related JS file to your page:
+
+        <script type="text/javascript" src="typer.js"></script>
+
+2. Run it by a single line of code:
+
+        new Typer(elementID, options);
+
+The first argument, `elementID`, is the ID of the slides container.
+
+The second and optional argument, `options`, sets the behavior of Typer. Here are default options:
+* **slideDelay** : Integer - Time delay between slides in millisecond. Default is 2000.
+* **typeDelay**  : Integer - Typing delay in millisecond. Default is 100.
+* **blinking**   : Integer - Number of cursor blinkings before typing starts. Default is 2.
+* **cursor**     : String  - The trailing text that simulates the cursor. Default is '_'.
+* **loop**       : Boolean - Looping over slides. Default is true.
+
+So you may change default options by setting global variable TyperOptions that affects on all created instances of Typer:
+
+    TyperOptions.typeDelay = 250;
+
+Also you can pass desired options to Typer. See the example below.
+
+Example
+-------
+    <html lang="en">
+    <head>
+       <script type="text/javascript" src="typer.js"></script>
+    </head>
+    <body>
+       <div id="container">
+      
+         <ul id="my_typer" style="display:none;">
+           <li>First list item.</li>
+           <li>Second list item.</li>
+           <li>Third list item.</li>
+           <li>Forth list item.</li>
+         </ul>
+      
+       </div>
+    </body>
+      
+    <script type="text/javascript">
+       var options = {slideDelay:3000, typeDelay:40};
+       new Typer('my_typer', options);
+    </script>
+  
+    </html>
+
+######Important:######
+*  The most inner tag of each slide must contains a pure text. DON'T include HTML tags (e.g. <b>) in your text.
+*  It's better to add inline style `display:none` to your slider to avoid the list to be displayed at startup.
---
+++ typer.js
@@ -0,0 +1,178 @@
+/**
+ * Typer - Pure Javascript Typing Slider, v1.0
+ * Copyright 2013 Mohsen Khahani
+ *
+ * Licensed under the MIT license
+ * Created on May 18, 2013
+ *
+ * Features:
+ *   - Nice typing effects
+ *   - Support XHTML slides
+ *   - Configurable typing speed
+ *   - Pause on mouse over
+ *
+ * http://mohsenkhahani.ir/typer
+ */
+
+/**
+ * Default configuration options
+ */
+var typerOptions = {
+    slideDelay : 2000,  /* Time delay between slides in millisecond */
+    typeDelay : 100,    /* Typing delay in millisecond */
+    blinking : 2,       /* Number of cursor blinkings before typing starts */
+    cursor : '_',       /* The trailing text that simulates the cursor */
+    loop : true         /* Looping over slides */
+};
+
+/**
+ * Typer base class
+ */
+function Typer(elementId, options) {
+    options = options || {};
+    this.typeDelay = (options.typeDelay !== undefined)? options.typeDelay : typerOptions.typeDelay;
+    this.slideDelay = (options.slideDelay !== undefined)? options.slideDelay : typerOptions.slideDelay;
+    this.blinking = (options.blinking !== undefined)? options.blinking : typerOptions.blinking;
+    this.cursor = (options.cursor !== undefined)? options.cursor : typerOptions.cursor;
+    this.loop = (options.loop !== undefined)? options.loop : typerOptions.loop;
+    this.slides = [];
+    this.timer = null;
+    this.typing = false;
+    this.playing = false;
+    this.currIdx = 0;
+
+    this.init(elementId);
+}
+
+/**
+ * Initiates typer
+ */
+Typer.prototype.init = function(id) {
+    function getInnerElement(el) {
+        if (el.children.length === 0) {
+            return el;
+        } else {
+            return getInnerElement(el.children[0]);
+        }
+    }
+
+    var self = this,
+        slider,
+        i;
+
+    if (document.readyState != 'complete') {
+        setTimeout(function() { self.init(id); }, 100);
+        return;
+    }
+
+    slider = this.slider = document.getElementById(id);
+    if (slider.children.length === 0) {
+        return;
+    }
+    for (i = 0; i < slider.children.length; i++) {
+        this.slides[i] = {};
+        this.slides[i].outer = slider.children[i];
+        this.slides[i].outer.style.display = 'none';
+        this.slides[i].inner = getInnerElement(slider.children[i]);
+        this.slides[i].text = this.slides[i].inner.innerHTML;
+    }
+    this.addEvents(slider);
+    slider.style.display = '';
+    this.playing = true;
+    this.play();
+};
+
+/**
+ * Starts typer
+ */
+Typer.prototype.play = function() {
+    var slide = this.slides[this.currIdx],
+        prevIdx = (this.currIdx === 0) ?
+            this.slides.length - 1 : this.currIdx - 1;
+    if (this.playing && !this.typing) {
+        this.typing = true;
+        this.slides[prevIdx].outer.style.display = 'none';
+        this.blink(slide, 0);
+        if (this.currIdx === this.slides.length - 1) {
+            if (this.loop) {
+                this.currIdx = 0;
+            } else {
+                this.playing = false;
+                this.removeEvents(this.slider);
+                return;
+            }
+        } else {
+            this.currIdx += 1;
+        }
+    }
+};
+
+/**
+ * Blinks cursor before type begins
+ */
+Typer.prototype.blink = function(slide, index) {
+    var self = this;
+    slide.outer.style.display = '';
+    if (index < this.blinking) {
+        slide.inner.innerHTML = (index % 2 === 0) ? this.cursor : '';
+        setTimeout(function() { self.blink(slide, index + 1); }, (500));
+    } else {
+        this.type(slide, 0);
+    }
+};
+
+/**
+ * Types inner text of the givven slide
+ */
+Typer.prototype.type = function(slide, index) {
+    var self = this;
+    slide.inner.innerHTML = slide.text.substr(0, index) + this.cursor;
+    if (index < slide.text.length) {
+        setTimeout(function() { self.type(slide, index + 1); }, (this.typeDelay));
+    } else {
+        slide.inner.innerHTML = slide.text;
+        this.typing = false;
+        if (this.playing) {
+            this.timer = setTimeout(function() {
+                self.play();
+            }, (this.slideDelay));
+        }
+    }
+};
+
+/**
+ * Adds pause & play functionality
+ */
+Typer.prototype.addEvents = function(el) {
+    var self = this;
+    this.resume = function() {
+        self.playing = true;
+        self.timer = setTimeout(function() {
+            self.play();
+        }, (1000));
+    };
+    this.pause = function() {
+        clearTimeout(self.timer);
+        self.playing = false;
+    };
+    if (el.addEventListener) {
+        el.addEventListener('mouseover', this.pause, false);
+        el.addEventListener('mouseout', this.resume, false);
+    } else {
+        el.attachEvent('onmouseover', this.pause);
+        el.attachEvent('onmouseout', this.resume);
+    }
+};
+
+/**
+ * Removes pause & play functionality
+ */
+Typer.prototype.removeEvents = function(el) {
+    if (el.removeEventListener) {
+        el.removeEventListener('mouseover', this.pause, false);
+        el.removeEventListener('mouseout', this.resume, false);
+    } else {
+        el.detachEvent('onmouseover', this.pause);
+        el.detachEvent('onmouseout', this.resume);
+    }
+};
